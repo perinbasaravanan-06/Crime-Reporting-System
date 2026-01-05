95c804a8e317d084e02dabd4637cb561bfaabeed
perinbasaravanan-06 <perinbasaravanank06@gmail.com>
2026-01-05 14:33:49 +0530

    Frontend link added to cors

--- crime-reporting-backend/src/main/java/com/crime/reporting/security/CorsConfig.java
+++ crime-reporting-backend/src/main/java/com/crime/reporting/security/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://crime-reporting-system-eta.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
